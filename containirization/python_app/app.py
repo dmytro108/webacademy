990b586cbb87df93f8b4a556318c06374e26fa4b
Dmitry Fedunets <dmitry.fedunets@gmail.com>
2023-08-22 15:53:39 +0000

    update dockerfile and compose file with environmen

--- containirization/python_app/app.py
+++ containirization/python_app/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify
 import redis
+import os
 
 app = Flask(__name__)
-redis_client = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+redis_host = os.getenv('REDIS_HOST', '127.0.0.1')
+redis_port = os.getenv('REDIS_PORT', 6379)
+
+redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
 
 @app.route('/redis/<key_name>', methods=['GET'])
 def get_redis_value(key_name):
